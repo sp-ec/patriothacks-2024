54eb03dd12e207bf3aca4a334dd4ff2bc53a00b0
Spectrobyte <48649161+Spectromeda@users.noreply.github.com>
2024-10-11 22:40:26 -0400

    patriothacks-2024

---
+++ src/pages/Main.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Main extends Component {
+    render() {
+        return (
+            <div>
+                <h1 className='text-green-600 bg-black'>Main</h1>
+            </div>
+        )
+    }
+}
+
+export default Main
