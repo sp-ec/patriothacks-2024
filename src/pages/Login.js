d88021765586ff3aa6ea8335e032531a7f86f6be
catherine-tomic <cattomic@gmail.com>
2024-10-12 09:49:18 -0400

    Login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,26 +3,24 @@ import React, { Component } from 'react'
 export class Login extends Component {
     render() {
         return (
-            <section class="bg-neutral-800">
-                <body >
-                    <div class="p-6 space-y-4 md:space-y-6 sm:p-8 mt-72">
-                        <h1 class="text-xl font-bold text-white md:text-4xl text-center">Login</h1>
-                        <form class="space-y-4 md:space-y-2 text-center">
-                            <div>
-                                <label for="username" class="block text-md text-purple-500">name</label>
-                                <input class="bg-gray-400 rounded-sm"></input>
+            <section >
+                <div className="max-w-md mx-auto bg-black rounded-lg border border-white p-6 space-y-4 md:space-y-6 sm:p-8 mt-72 shadow-lg">
+                    <h1 className="text-xl font-bold text-white md:text-4xl text-center">Login</h1>
+                    <form className="space-y-4 md:space-y-2 text-center">
+                        <div>
+                            <label for="username" className="block text-md text-purple-500">name</label>                                
+                            <input id="username" class="bg-gray-400 rounded-sm"></input>
                             </div>
                             <div>
-                                <label for="username" class="block text-md text-purple-500">password</label>
-                                <input class="bg-gray-400 rounded-sm"></input>
+                                <label for="password" type="text" className="block text-md text-purple-500">password</label>
+                                <input id="password" class="bg-gray-400 rounded-sm"></input>
                             </div>
-                            <div class="flex-row">
-                                    <input type="checkbox" class="appearance-none bg-gray-400 w-4 h-4 rounded-sm"></input>
-                                    <label class="text-sm text-blue-400 ml-3">remember me</label>
-                            </div>
-                        </form>
-                    </div>
-                </body>
+                            <div className="flex-row">
+                                <input type="checkbox" className="appearance-none bg-gray-400 w-4 h-4 rounded-sm"></input>
+                                <label type="text" className="text-sm text-blue-400 ml-3">remember me</label>
+                        </div>
+                    </form>
+                </div>
             </section>
         )
     }
