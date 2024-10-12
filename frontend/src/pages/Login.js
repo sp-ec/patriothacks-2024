d7c04ba9154ef01b749d0794f155a209156c5a1f
catherine-tomic <cattomic@gmail.com>
2024-10-12 16:52:28 -0400

    Checkbox Still Doesn't Work

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -3,6 +3,29 @@ import cityscapeImage from '../images/sky.webp';
 import '../App.css';
 
 export class Login extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: '',
+            password: '',
+            rememberMe: false
+        };
+    }
+
+    handleCheckboxChange = (event) => {
+        this.setState({ rememberMe: event.target.checked });
+    };
+
+    handleInputChange = (event) => {
+        const { name, value } = event.target;
+        this.setState({ [name]: value});
+    };
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(this.state);
+    }
+
     render() {
         const backgroundStyle = {
             backgroundImage: `url(${cityscapeImage})`,
@@ -21,18 +44,18 @@ export class Login extends Component {
                     <form className="space-y-4 md:space-y-2 text-center">
                         <div className="mt-12">
                             <label for="username" className="block text-xl text-neonPurple">name</label>
-                            <input id="username" className="bg-gray-400 rounded-bl-lg rounded-tr-lg"></input>
+                            <input id="username" name="username" value={this.state.username} onChange={this.handleInputChange} className="bg-gray-400 rounded-bl-lg rounded-tr-lg"></input>
                         </div>
                         <div>
                             <label for="password" type="password" className="block text-xl text-neonPurple">password</label>
-                            <input id="password" className="bg-gray-400 rounded-bl-lg rounded-tr-lg"></input>
+                            <input id="password" name="password" type="password" value={this.state.password} onChange={this.handleInputChange} className="bg-gray-400 rounded-bl-lg rounded-tr-lg"></input>
 
                             <div className="mt-3 flex-row">
-                                <input id="checkbox" type="checkbox" value="" className="appearance-none bg-gray-400 w-4 h-4 rounded-bl-sm rounded-tr-sm"></input>
-                                <label for="checkbox" type="text" className="text-sm text-neonBlue ml-3">remember me</label>
+                                <input id="checkbox" type="checkbox" checked={this.state.rememberMe} onChange={this.handleCheckboxChange} className="appearance-none bg-gray-400 w-4 h-4 rounded-bl-sm rounded-tr-sm"></input>
+                                <label htmlFor="checkbox" type="text" className="text-sm text-neonBlue ml-3">remember me</label>
 
                                 <div className="mt-6">
-                                    <button type="submit" className="w-3xl min-w-[200px] bg-neutral-600 text-neonBlue rounded-bl-lg rounded-tr-lg py-1 hover:bg-neonPurple">Login</button>
+                                    <button type="submit" className="w-3xl min-w-[200px] bg-neutral-700 text-neonBlue rounded-bl-lg rounded-tr-lg py-1 hover:bg-neonPurple">Login</button>
                                 </div>
                                 <div className="mt-6">
                                     <p className="text-sm text-neonPurple">new here?</p>
