cffe0e75a111f457eff6e69865795c792e88728b
catherine-tomic <cattomic@gmail.com>
2024-10-12 00:21:29 -0400

    Login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -6,10 +6,12 @@ export class Login extends Component {
             <section class="bg-neutral-800">
                 <body >
                     <div class="p-6 space-y-4 md:space-y-6 sm:p-8 mt-72">
-                        <h1 class="text-xl font-bold text-white md:text-4xl">Login</h1>
-                        <form class="space-y-4 md:space-y-6" action="#">
-                            <p class="text-purple-500">name</p>
-                            <p class="text-purple-500">password</p>
+                        <h1 class="text-xl font-bold text-white md:text-4xl text-center">Login</h1>
+                        <form class="space-y-4 md:space-y-6 text-center" action="#">
+                            <label for="username" class="block text-md text-purple-500">name</label>
+                            <input></input>
+                            <label for="username" class="block text-md text-purple-500">password</label>
+                            <input></input>
                         </form>
                     </div>
                 </body>
