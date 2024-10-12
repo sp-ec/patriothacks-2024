82b63d4c650a3e322b9a19072f505864f5f9fe3d
catherine-tomic <cattomic@gmail.com>
2024-10-12 01:10:33 -0400

    Login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -10,17 +10,15 @@ export class Login extends Component {
                         <form class="space-y-4 md:space-y-2 text-center">
                             <div>
                                 <label for="username" class="block text-md text-purple-500">name</label>
-                                <input class="bg-gray-400"></input>
+                                <input class="bg-gray-400 rounded-sm"></input>
                             </div>
                             <div>
                                 <label for="username" class="block text-md text-purple-500">password</label>
-                                <input class="bg-gray-400"></input>
+                                <input class="bg-gray-400 rounded-sm"></input>
                             </div>
-                            <div>
-                                    <input type="checkbox" class="w-4 h-4"></input>
-                                    <div class="ml-3">
-                                        <label class="text-sm text-blue-400">remember me</label>
-                                    </div>
+                            <div class="flex-row">
+                                    <input type="checkbox" class="appearance-none bg-gray-400 w-4 h-4 rounded-sm"></input>
+                                    <label class="text-sm text-blue-400 ml-3">remember me</label>
                             </div>
                         </form>
                     </div>
