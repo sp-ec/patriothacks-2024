3967cf4873ac8d9dba5093e4c25b590d719fea73
catherine-tomic <cattomic@gmail.com>
2024-10-12 22:53:27 -0400

    mac bullshit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route exact path="/" element={<Login />} />  {/* The login page */}
+        <Route exact path="/" element={<Main />} />  {/* The login page */}
         <Route path="/register" element={<Register />} />  {/* The register  page */}
       </Routes>
     </Router>
